c580922dc1a0460720013c9b9d49ca78ca386276
krutikamandanka <krutikamandanka0462@gmail.com.com>
2022-01-16 17:12:04 +0530

    fix

--- app/Http/Controllers/Student/FeedbackController.php
+++ app/Http/Controllers/Student/FeedbackController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers\Student;
 use App\Http\Controllers\Controller;
+use Carbon\Carbon;
 
 use App\Models\Feedback;
+use App\Models\Bookings;
 
 use Illuminate\Http\Request;
 
@@ -26,7 +28,26 @@ class FeedbackController extends Controller
      */
     public function index()
     {
+
         $feedbacks = Feedback::with(['tutor'])->where('user_id', auth()->user()->id)->orderBy('id','desc')->get();
-        return view('student.feedback.index',compact('feedbacks'));
+        $booking_slot=Bookings::with(['tutor'])->whereDate('date_time','<',Carbon::now())->where('user_id',auth()->user()->id)->get();
+        
+        return view('student.feedback.index',compact('feedbacks','booking_slot'));
+    }
+    public function store(Request $request)
+    {
+        $feedbacks = Feedback::where('user_id', auth()->user()->id)->where('tutor_id',$request->tutor_id)->orderBy('id','desc')->get();
+        if(isset($feedbacks)){
+                return redirect()->back()->with('danger', 'Already feedback added.');
+        }else{
+            $feedback=new Feedback();
+            $feedback->tutor_id=$request->tutor_id;
+            $feedback->user_id= auth()->user()->id;
+            $feedback->description=$request->description;
+            $feedback->rating=1;
+            $feedback->save();
+            return redirect()->back()->with('success', 'Feedback Added Successfully');
+        }
+        
     }
 }
